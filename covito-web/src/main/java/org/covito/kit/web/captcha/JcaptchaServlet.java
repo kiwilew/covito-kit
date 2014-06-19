aca984d1e3385d0934b61263b8c148aa9910cf56
covito <covito@163.com>
2014-06-19 11:22:50 +0800

    servlet config

--- covito-web/src/main/java/org/covito/kit/web/captcha/JcaptchaServlet.java
+++ covito-web/src/main/java/org/covito/kit/web/captcha/JcaptchaServlet.java
@@ -2,8 +2,10 @@ package org.covito.kit.web.captcha;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.covito.kit.utility.ObjectUtil;
+import org.covito.kit.web.utility.HttpCompUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
@@ -23,16 +29,49 @@ import com.octo.captcha.service.image.ImageCaptchaService;
  */
 @SuppressWarnings("serial")
 public class JcaptchaServlet extends HttpServlet {
+	
+	Logger logger=LoggerFactory.getLogger(getClass());
+	
 	public static final String CAPTCHA_IMAGE_FORMAT = "jpeg";
 
 	private ImageCaptchaService captchaService;
+	
+	
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @author  covito
+	 * @param config
+	 * @throws ServletException
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		WebApplicationContext appCtx = WebApplicationContextUtils
+				.getWebApplicationContext(getServletContext());
+		Map<String,ImageCaptchaService> m= BeanFactoryUtils.beansOfTypeIncludingAncestors(
+				appCtx, ImageCaptchaService.class);
+		if(ObjectUtil.empty(m)){
+			logger.error("spring config not found com.octo.captcha.service.image.ImageCaptchaService impl");
+			return;
+		}
+		String imageName=HttpCompUtil.getSvlConfig(config, "captchaService", null);
+		if(imageName==null&&m.size()==1){
+			captchaService=m.values().iterator().next();
+		}else{
+			captchaService=m.get(imageName);
+		}
+		
+		if(captchaService==null){
+			logger.error("spring config ImageCaptchaService impl not only or captchaService not found");
+			return;
+		}
+	}
 
 	@Override
 	public void init() throws ServletException {
-		WebApplicationContext appCtx = WebApplicationContextUtils
-				.getWebApplicationContext(getServletContext());
-		captchaService = (ImageCaptchaService) BeanFactoryUtils.beanOfTypeIncludingAncestors(
-				appCtx, ImageCaptchaService.class);
+		
+		
 	}
 
 	@Override
--- covito-web/src/main/java/org/covito/kit/web/file/FileDownLoadServlet.java
+++ covito-web/src/main/java/org/covito/kit/web/file/FileDownLoadServlet.java
@@ -2,7 +2,10 @@ package org.covito.kit.web.file;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,8 +14,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.covito.kit.file.FileMeta;
 import org.covito.kit.file.FileService;
 import org.covito.kit.file.FileServiceUtil;
+import org.covito.kit.utility.ObjectUtil;
+import org.covito.kit.web.utility.HttpCompUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.octo.captcha.service.image.ImageCaptchaService;
 
 public class FileDownLoadServlet extends HttpServlet {
 	/**
@@ -24,36 +34,87 @@ public class FileDownLoadServlet extends HttpServlet {
 
 	protected FileService fileService;
 
-	protected final String[] ext = { "ez", "hqx", "cpt", "doc", "bin", "dms", "lha", "lzh", "exe", "class", "so", "dll", "oda", "pdf", "ai", "eps", "ps",
-			"smi", "smil", "mif", "xls", "ppt", "wbxml", "wmlc", "wmlsc", "bcpio", "vcd", "pgn", "cpio", "csh", "dcr", "dir", "dxr", "dvi", "spl", "gtar",
-			"hdf", "js", "skp", "skd", "skt", "skm", "latex", "nc", "cdf", "sh", "shar", "swf", "sit", "sv4cpio", "sv4crc", "tar", "tcl", "tex", "texinfo",
-			"texi", "t", "tr", "roff", "man", "me", "ms", "ustar", "src", "xhtml", "xht", "zip", "au", "snd", "mid", "midi", "kar", "mpga", "mp2", "mp3",
-			"aif", "aiff", "aifc", "m3u", "ram", "rm", "rpm", "ra", "wav", "pdb", "xyz", "bmp", "gif", "ief", "jpeg", "jpg", "jpe", "png", "tiff", "tif",
-			"djvu", "djv", "wbmp", "ras", "pnm", "pbm", "pgm", "ppm", "rgb", "xbm", "xpm", "xwd", "igs", "iges", "msh", "mesh", "silo", "wrl", "vrml", "css",
-			"html", "htm", "asc", "txt", "rtx", "rtf", "sgml", "sgm", "tsv", "wml", "wmls", "etx", "xsl", "xml", "mpeg", "mpg", "mpe", "qt", "mov", "mxu",
-			"avi", "movie", "ice" };
+	protected final String[] ext = { "ez", "hqx", "cpt", "doc", "bin", "dms", "lha", "lzh", "exe",
+			"class", "so", "dll", "oda", "pdf", "ai", "eps", "ps", "smi", "smil", "mif", "xls",
+			"ppt", "wbxml", "wmlc", "wmlsc", "bcpio", "vcd", "pgn", "cpio", "csh", "dcr", "dir",
+			"dxr", "dvi", "spl", "gtar", "hdf", "js", "skp", "skd", "skt", "skm", "latex", "nc",
+			"cdf", "sh", "shar", "swf", "sit", "sv4cpio", "sv4crc", "tar", "tcl", "tex", "texinfo",
+			"texi", "t", "tr", "roff", "man", "me", "ms", "ustar", "src", "xhtml", "xht", "zip",
+			"au", "snd", "mid", "midi", "kar", "mpga", "mp2", "mp3", "aif", "aiff", "aifc", "m3u",
+			"ram", "rm", "rpm", "ra", "wav", "pdb", "xyz", "bmp", "gif", "ief", "jpeg", "jpg",
+			"jpe", "png", "tiff", "tif", "djvu", "djv", "wbmp", "ras", "pnm", "pbm", "pgm", "ppm",
+			"rgb", "xbm", "xpm", "xwd", "igs", "iges", "msh", "mesh", "silo", "wrl", "vrml", "css",
+			"html", "htm", "asc", "txt", "rtx", "rtf", "sgml", "sgm", "tsv", "wml", "wmls", "etx",
+			"xsl", "xml", "mpeg", "mpg", "mpe", "qt", "mov", "mxu", "avi", "movie", "ice" };
 
-	protected final String[] contentType = { "application/andrew-inset", "application/mac-binhex40", "application/mac-compactpro", "application/msword",
-			"application/octet-stream", "application/octet-stream", "application/octet-stream", "application/octet-stream", "application/octet-stream",
-			"application/octet-stream", "application/octet-stream", "application/octet-stream", "application/oda", "application/pdf", "application/postscript",
-			"application/postscript", "application/postscript", "application/smil", "application/smil", "application/vnd.mif", "application/vnd.ms-excel",
-			"application/vnd.ms-powerpoint", "application/vnd.wap.wbxml", "application/vnd.wap.wmlc", "application/vnd.wap.wmlscriptc", "application/x-bcpio",
-			"application/x-cdlink", "application/x-chess-pgn", "application/x-cpio", "application/x-csh", "application/x-director", "application/x-director",
-			"application/x-director", "application/x-dvi", "application/x-futuresplash", "application/x-gtar", "application/x-hdf", "application/x-javascript",
-			"application/x-koan", "application/x-koan", "application/x-koan", "application/x-koan", "application/x-latex", "application/x-netcdf",
-			"application/x-netcdf", "application/x-sh", "application/x-shar", "application/x-shockwave-flash", "application/x-stuffit",
-			"application/x-sv4cpio", "application/x-sv4crc", "application/x-tar", "application/x-tcl", "application/x-tex", "application/x-texinfo",
-			"application/x-texinfo", "application/x-troff", "application/x-troff", "application/x-troff", "application/x-troff-man", "application/x-troff-me",
-			"application/x-troff-ms", "application/x-ustar", "application/x-wais-source", "application/xhtml+xml", "application/xhtml+xml", "application/zip",
-			"audio/basic", "audio/basic", "audio/midi", "audio/midi", "audio/midi", "audio/mpeg", "audio/mpeg", "audio/mpeg", "audio/x-aiff", "audio/x-aiff",
-			"audio/x-aiff", "audio/x-mpegurl", "audio/x-pn-realaudio", "audio/x-pn-realaudio", "audio/x-pn-realaudio-plugin", "audio/x-realaudio",
-			"audio/x-wav", "chemical/x-pdb", "chemical/x-xyz", "image/bmp", "image/gif", "image/ief", "image/jpeg", "image/jpeg", "image/jpeg", "image/png",
-			"image/tiff", "image/tiff", "image/vnd.djvu", "image/vnd.djvu", "image/vnd.wap.wbmp", "image/x-cmu-raster", "image/x-portable-anymap",
-			"image/x-portable-bitmap", "image/x-portable-graymap", "image/x-portable-pixmap", "image/x-rgb", "image/x-xbitmap", "image/x-xpixmap",
-			"image/x-xwindowdump", "model/iges", "model/iges", "model/mesh", "model/mesh", "model/mesh", "model/vrml", "model/vrml", "text/css", "text/html",
-			"text/html", "text/plain", "text/plain", "text/richtext", "text/rtf", "text/sgml", "text/sgml", "text/tab-separated-values", "text/vnd.wap.wml",
-			"text/vnd.wap.wmlscript", "text/x-setext", "text/xml", "text/xml", "video/mpeg", "video/mpeg", "video/mpeg", "video/quicktime", "video/quicktime",
-			"video/vnd.mpegurl", "video/x-msvideo", "video/x-sgi-movie", "x-conference/x-cooltalk" };
+	protected final String[] contentType = { "application/andrew-inset",
+			"application/mac-binhex40", "application/mac-compactpro", "application/msword",
+			"application/octet-stream", "application/octet-stream", "application/octet-stream",
+			"application/octet-stream", "application/octet-stream", "application/octet-stream",
+			"application/octet-stream", "application/octet-stream", "application/oda",
+			"application/pdf", "application/postscript", "application/postscript",
+			"application/postscript", "application/smil", "application/smil",
+			"application/vnd.mif", "application/vnd.ms-excel", "application/vnd.ms-powerpoint",
+			"application/vnd.wap.wbxml", "application/vnd.wap.wmlc",
+			"application/vnd.wap.wmlscriptc", "application/x-bcpio", "application/x-cdlink",
+			"application/x-chess-pgn", "application/x-cpio", "application/x-csh",
+			"application/x-director", "application/x-director", "application/x-director",
+			"application/x-dvi", "application/x-futuresplash", "application/x-gtar",
+			"application/x-hdf", "application/x-javascript", "application/x-koan",
+			"application/x-koan", "application/x-koan", "application/x-koan",
+			"application/x-latex", "application/x-netcdf", "application/x-netcdf",
+			"application/x-sh", "application/x-shar", "application/x-shockwave-flash",
+			"application/x-stuffit", "application/x-sv4cpio", "application/x-sv4crc",
+			"application/x-tar", "application/x-tcl", "application/x-tex", "application/x-texinfo",
+			"application/x-texinfo", "application/x-troff", "application/x-troff",
+			"application/x-troff", "application/x-troff-man", "application/x-troff-me",
+			"application/x-troff-ms", "application/x-ustar", "application/x-wais-source",
+			"application/xhtml+xml", "application/xhtml+xml", "application/zip", "audio/basic",
+			"audio/basic", "audio/midi", "audio/midi", "audio/midi", "audio/mpeg", "audio/mpeg",
+			"audio/mpeg", "audio/x-aiff", "audio/x-aiff", "audio/x-aiff", "audio/x-mpegurl",
+			"audio/x-pn-realaudio", "audio/x-pn-realaudio", "audio/x-pn-realaudio-plugin",
+			"audio/x-realaudio", "audio/x-wav", "chemical/x-pdb", "chemical/x-xyz", "image/bmp",
+			"image/gif", "image/ief", "image/jpeg", "image/jpeg", "image/jpeg", "image/png",
+			"image/tiff", "image/tiff", "image/vnd.djvu", "image/vnd.djvu", "image/vnd.wap.wbmp",
+			"image/x-cmu-raster", "image/x-portable-anymap", "image/x-portable-bitmap",
+			"image/x-portable-graymap", "image/x-portable-pixmap", "image/x-rgb",
+			"image/x-xbitmap", "image/x-xpixmap", "image/x-xwindowdump", "model/iges",
+			"model/iges", "model/mesh", "model/mesh", "model/mesh", "model/vrml", "model/vrml",
+			"text/css", "text/html", "text/html", "text/plain", "text/plain", "text/richtext",
+			"text/rtf", "text/sgml", "text/sgml", "text/tab-separated-values", "text/vnd.wap.wml",
+			"text/vnd.wap.wmlscript", "text/x-setext", "text/xml", "text/xml", "video/mpeg",
+			"video/mpeg", "video/mpeg", "video/quicktime", "video/quicktime", "video/vnd.mpegurl",
+			"video/x-msvideo", "video/x-sgi-movie", "x-conference/x-cooltalk" };
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @author covito
+	 * @throws ServletException
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		WebApplicationContext appCtx = WebApplicationContextUtils
+				.getWebApplicationContext(getServletContext());
+		Map<String, FileService> m = BeanFactoryUtils.beansOfTypeIncludingAncestors(appCtx,
+				FileService.class);
+		if (ObjectUtil.empty(m)) {
+			logger.error("spring config not found FileService impl");
+			return;
+		}
+		String imageName = HttpCompUtil.getSvlConfig(config, "fileService", null);
+		if (imageName == null && m.size() == 1) {
+			fileService = m.values().iterator().next();
+		} else {
+			fileService = m.get(imageName);
+		}
+
+		if (fileService == null) {
+			logger.error("spring config fileService impl not only or fileService not found");
+			return;
+		}
+	}
 
 	/**
 	 * {@inheritDoc}
@@ -95,19 +156,19 @@ public class FileDownLoadServlet extends HttpServlet {
 	 */
 	protected void outputFile(HttpServletResponse response, String path) throws IOException {
 		FileMeta info = fileService.getFileInfo(path);
-		String fileName=info.getFileName();
+		String fileName = info.getFileName();
 		Long size = info.getFileSize();
 		response.setContentLength(size.intValue());
 		response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
-		String extname=fileName.substring(fileName.lastIndexOf(".")+1, fileName.length());
-		int index=-1;
-		for(int i=0;i<ext.length;i++){
-			if(ext[i].equalsIgnoreCase(extname)){
-				index=i;
+		String extname = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
+		int index = -1;
+		for (int i = 0; i < ext.length; i++) {
+			if (ext[i].equalsIgnoreCase(extname)) {
+				index = i;
 				break;
 			}
 		}
-		if(index!=-1){
+		if (index != -1) {
 			response.setContentType(contentType[index]);
 		}
 		ServletOutputStream os = response.getOutputStream();
